f01cc028fcd7c3398f2b4508f2b8165f08d1b32b
Joao Szerman <joaoszerman@gmail.com>
2022-10-05 11:25:13 -0300

    initial gridview products

--- lib/pages/products_overview_page.dart
+++ lib/pages/products_overview_page.dart
@@ -2,11 +2,32 @@ import 'package:flutter/material.dart';
 import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter/src/widgets/framework.dart';
 
+import '../data/dummy_data.dart';
+import '../models/product.dart';
+
 class ProductsOverviewPage extends StatelessWidget {
-  const ProductsOverviewPage({Key? key}) : super(key: key);
+  final List<Product> loadedProducts = dummtProducts;
+  ProductsOverviewPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Minha loja'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(10),
+        child: GridView.builder(
+          itemBuilder: (ctx, i) => Text(loadedProducts[i].title),
+          itemCount: loadedProducts.length,
+          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2,
+            childAspectRatio: 3 / 2,
+            crossAxisSpacing: 10,
+            mainAxisSpacing: 10,
+          ),
+        ),
+      ),
+    );
   }
 }
