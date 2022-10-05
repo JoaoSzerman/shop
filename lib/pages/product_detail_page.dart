a5bb060c3b3d9ce8dacd94416f32b1e472922e26
Joao Szerman <joaoszerman@gmail.com>
2022-10-05 13:46:35 -0300

    created product detail page

---
+++ lib/pages/product_detail_page.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:shop/models/product.dart';
+
+class ProductDetailPage extends StatelessWidget {
+  final Product product;
+  const ProductDetailPage({
+    Key? key,
+    required this.product,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(product.title),
+      ),
+    );
+  }
+}
