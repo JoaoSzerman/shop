d3736396cea2bbae4daaa3e9b5a7f1794ed8d82f
Joao Szerman <joaoszerman@gmail.com>
2022-10-05 10:42:21 -0300

    dummy dara created

--- lib/models/product.dart
+++ lib/models/product.dart
@@ -3,7 +3,7 @@ class Product {
   final String title;
   final String description;
   final double price;
-  final String imagrUrl;
+  final String imageUrl;
   bool isFavorite;
 
   Product({
@@ -11,7 +11,7 @@ class Product {
     required this.title,
     required this.description,
     required this.price,
-    required this.imagrUrl,
+    required this.imageUrl,
     this.isFavorite = false,
   });
 
