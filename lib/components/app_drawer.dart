872e1fd74fe4b6e6f9ab05b7ba95089857bcdc3b
Joao Szerman <joaoszerman@gmail.com>
2022-10-17 11:13:48 -0300

    drawer, confirm delete, product grid and card

--- lib/components/app_drawer.dart
+++ lib/components/app_drawer.dart
@@ -12,20 +12,20 @@ class AppDrawer extends StatelessWidget {
       child: Column(
         children: [
           AppBar(
-            title: Text('Bem vindo'),
+            title: const Text('Bem vindo'),
           ),
-          Divider(),
+          const Divider(),
           ListTile(
-            leading: Icon(Icons.shop),
-            title: Text("Loja"),
+            leading: const Icon(Icons.shop),
+            title: const Text("Loja"),
             onTap: () {
               Navigator.of(context).pushReplacementNamed(AppRoutes.home);
             },
           ),
-          Divider(),
+          const Divider(),
           ListTile(
-            leading: Icon(Icons.payment),
-            title: Text("Pedidos"),
+            leading: const Icon(Icons.payment),
+            title: const Text("Pedidos"),
             onTap: () {
               Navigator.of(context).pushReplacementNamed(AppRoutes.orders);
             },
